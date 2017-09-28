b05a229df0187ce6db6b7f327f7d89fd5a3b8d51
Eliza Brine <email": null>
2017-09-28 15:23:27 -0700

    Adds bcrypt require to env. Adds password hash creation methods to User class

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,4 +1,16 @@
 class User < ApplicationRecord
+  include BCrypt
+
   validates :username, :email, presence: true, uniqueness: true
   validates :password_hash, presence: true
+
+  def password
+    @password ||= Password.new(password_hash)
+  end
+
+  def password=(new_password)
+    @password = Password.create(new_password)
+    self.password_hash = @password
+  end
+
 end
