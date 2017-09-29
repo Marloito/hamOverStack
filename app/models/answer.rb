83d744272d65ccf9b736c58f28fd6f34fa778d5e
Eliza Brine & Mandil <pair+ebrine.mandil@devbootcamp.com>
2017-09-29 12:35:46 -0700

    Adds uniqueness validation to answer, such that a user can only answer a particular question one time

--- app/models/answer.rb
+++ app/models/answer.rb
@@ -2,4 +2,6 @@ class Answer < ApplicationRecord
   validates :description, :user, :question, presence: true
   belongs_to :user
   belongs_to :question
+
+  validates :question, uniqueness: {scope: :user}
 end
