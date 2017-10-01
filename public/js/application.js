73a0c33f63536f9130894043f2ae22c75f4ed969
Marlo Ito <email": null>
2017-10-01 15:45:08 -0700

    shows answer -- clears text box -- creates answer -- WILL WORK ON ERROR HANDLING

--- public/js/application.js
+++ public/js/application.js
@@ -1,5 +1,19 @@
 $(document).ready(() => {
   $('#answer-form').submit((e) => {
     e.preventDefault();
+    const url = $(e.target).attr('action')
+    const data = $(e.target).find('textarea').serialize();
+
+    request = $.ajax({
+      method: "POST",
+      url,
+      data
+    });
+
+    request.done((response) => {
+      console.log(response)
+      $('#all-answers').html(response);
+      $(e.target).find('textarea').val("");
+    });
   });
 });
