24b810b6eaeb2494c32aacfa74e57679190722c8
Blaine Anderson <BlaineA97@Gmail.com>
2017-09-30 22:11:14 -0700

    selecting best answer is ajaxed, deselecting is not yet

--- app/controllers/answer.rb
+++ app/controllers/answer.rb
@@ -1,18 +1,38 @@
 post '/answers/:id' do
   answer = Answer.find(params[:id])
   question = Question.find(answer.question_id)
-  if answer.best_answer
-    answer.best_answer = false
-    answer.save
-    redirect "/questions/#{question.id}"
-  else
-    question.answers.each do |ans|
-      ans.best_answer = false
-      ans.save
+  #Not currently working Ajax
+  if request.xhr?
+    if answer.best_answer
+      answer.best_answer = false
+      answer.save
+      p "*" * 100
+      erb :'/questions/_show', :locals => {question: question, answer: answer}, :layout => false
+    else
+      question.answers.each do |ans|
+        ans.best_answer = false
+        ans.save
+      end
+      answer.best_answer = true
+      answer.save
+      p "~" * 100
+      erb :'/questions/_show', :locals => {question: question, answer: answer}, :layout => false
     end
-    answer.best_answer = true
-    answer.save
-    redirect "/questions/#{question.id}"
+  #if its ajax then dynamically load the best_answer
+  else
+    if answer.best_answer
+      answer.best_answer = false
+      answer.save
+      redirect "/questions/#{question.id}"
+    else
+      question.answers.each do |ans|
+        ans.best_answer = false
+        ans.save
+      end
+      answer.best_answer = true
+      answer.save
+      redirect "/questions/#{question.id}"
+    end
   end
 end
 
--- public/js/application.js
+++ public/js/application.js
@@ -1,6 +1,14 @@
-$(document).ready(() => {
-  // $('#new-user-form').submit( (e) => {
-  //   e.preventDefault();
-  //
-  // })
+$(document).ready( () => {
+  $('.select-best-answer').on('submit', (e) => {
+    e.preventDefault();
+    const url_id = $(e.target).attr('action');
+  $.ajax({
+    method: 'post',
+    url: url_id
+  }).done( (response) => {
+      console.log(response);
+      $('.best-answer').html(response);
+      $('.select-best-answer').hide();
+    })
+  })
 });
