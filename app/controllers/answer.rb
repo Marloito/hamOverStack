f8bb745c90a357259611a088bd78fb9f43ca520e
Marlo Ito <email": null>
2017-09-30 18:24:13 -0700

    updated answer controller to make answer a global variable

--- app/controllers/answer.rb
+++ app/controllers/answer.rb
@@ -19,7 +19,7 @@ end
 post '/questions/:question_id/answers' do
   @answer = Answer.create(description: params[:description], user_id: session[:user_id], question_id: params[:question_id])
 
-  @error_messages = answer.errors.full_messages
+  @error_messages = @answer.errors.full_messages
   if @error_messages.length > 0
     @question = Question.find(params[:question_id])
     erb  :"/questions/show"
