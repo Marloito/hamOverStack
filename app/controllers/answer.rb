8f935744bae3c0ba8e7f1e81de0d9ef20599b185
Blaine Anderson <BlaineA97@Gmail.com>
2017-09-30 18:14:05 -0700

    refactored code in order to create 2 submit buttons. User may select best answer and deselect best answer. buttons will only appear to the original user who posted the question. Buttons will only appear provided there is or is not a best answer

--- app/controllers/answer.rb
+++ app/controllers/answer.rb
@@ -1,15 +1,23 @@
-get '/answers/:id' do
+post '/answers/:id' do
   answer = Answer.find(params[:id])
-  question.answers.each do |ans|
-    ans.best_answer = false
-    ans.save
+  question = Question.find(answer.question_id)
+  if answer.best_answer
+    answer.best_answer = false
+    answer.save
+    redirect "/questions/#{question.id}"
+  else
+    question.answers.each do |ans|
+      ans.best_answer = false
+      ans.save
+    end
+    answer.best_answer = true
+    answer.save
+    redirect "/questions/#{question.id}"
   end
-  answer.best_answer = true
-  answer.save
-  redirect "/questions/#{question.id}"
 end
+
 post '/questions/:question_id/answers' do
-  answer = Answer.create(description: params[:description], user_id: session[:user_id], question_id: params[:question_id])
+  @answer = Answer.create(description: params[:description], user_id: session[:user_id], question_id: params[:question_id])
 
   @error_messages = answer.errors.full_messages
   if @error_messages.length > 0
--- app/controllers/question.rb
+++ app/controllers/question.rb
@@ -2,6 +2,7 @@ get '/questions' do
   @questions = Question.all
   erb :'/questions/index'
 end
+
 get '/questions/new' do
   erb :'/questions/new'
 end
