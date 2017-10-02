0dfd12bf0f61813b550964e60d6f53be8e29649e
Eliza Brine <email": null>
2017-10-01 21:07:00 -0700

    Implements user commenting for questions and answers

--- app/controllers/comment.rb
+++ app/controllers/comment.rb
@@ -1,5 +1,14 @@
 post '/questions/:id' do
-  question = Question.find(params[:id])
-  question.comments << Comment.create(comment_body: params[:comment_body], user_id: session[:user_id])
-  redirect "/questions/#{params[:id]}"
+  puts "*" * 100
+  type =  params[:commentable_type].strip
+  if type == "Question"
+    p "#" * 100
+    question = Question.find(params[:id])
+    question.comments << Comment.create(comment_body: params[:comment_body], user_id: session[:user_id])
+  elsif type == "Answer"
+    answer_id = params[:answer_id].strip
+    answer = Answer.find(answer_id)
+    answer.comments << Comment.create(comment_body: params[:comment_body], user_id: session[:user_id])
+  end
+    redirect "/questions/#{params[:id]}"
 end
